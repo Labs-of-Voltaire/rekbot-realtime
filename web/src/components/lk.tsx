02daea8c2dbeba1bf2081f2d4c25e5d1a4650b7a
pezzullig <giulio@voltairelabs.co.uk>
2024-10-08 11:39:12 +0100

    added logo url

--- web/src/components/lk.tsx
+++ web/src/components/lk.tsx
@@ -5,7 +5,7 @@ import Logo from "@/assets/lk.svg";
 export default function LK() {
   return (
     <a
-      href="#"
+      href="https://rekbot.ai"
       className="hover:opacity-70 transition-all duration-250 flex items-center gap-2"
       target="_blank"
       rel="noopener noreferrer"
