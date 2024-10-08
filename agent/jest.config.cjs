1f425cd04e3dc68ba977f4344abd95c24794c6be
pezzullig <giulio@voltairelabs.co.uk>
2024-10-08 01:29:26 +0100

    removed js side

--- agent/jest.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/test/**/*.test.ts'], // You can modify this to match the structure of your test folder
-    moduleFileExtensions: ['ts', 'js'],
-  };
--- agent/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-    interface ProcessEnv {
-      LIVEKIT_URL: string;
-      LIVEKIT_API_KEY: string;
-      LIVEKIT_API_SECRET: string;
-      OPENAI_API_KEY: string;
-      NODE_ENV: 'development' | 'production';
-    }
-  }
